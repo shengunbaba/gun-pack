9310d2a9dbcd72ada6793be5929cd0eb64f0558d
zhangli <zhangli@yxt.com>
2023-01-18 11:29:17 +0800

    wu

--- publish.js
+++ publish.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 
-const {copy} = require('./_node_modules/gun-pack/lib/utils');
+const {copy} = require('./_node_modules/wu-pack/lib/utils');
 
 const origin = path.join(__dirname, './_node_modules/gun-pack/lib');
 const target = path.join(__dirname, './lib');
